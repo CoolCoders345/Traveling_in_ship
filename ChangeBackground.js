53f5c56a7bd6629b92c09b1d9c9edcfc52558656
CoolCoders345 <86123958+CoolCoders345@users.noreply.github.com>
2021-06-19 20:17:19 -0700

    Add files via upload

---
+++ ChangeBackground.js
@@ -0,0 +1,71 @@
+var c1, c2, c3, c4, c5, c6,c7,c8,c9
+function setup(){
+    createCanvas(800,800)
+c1. createButton("RED")
+c1.position (10,200)
+c1.mousePressed(red)
+
+c2.createButton("GREEN")
+c2.position(100,200)
+c2.mousePressed(green)
+
+c3.createButton("BLUE")
+c3.position(200,200)
+c3.mousePressed(blue)
+
+c4.createButton("PURPLE")
+c4.position(300,200)
+c4.mousePressed(purple)
+
+c5.createButton("PINK")
+c5.position(400,200)
+c5.mousePressed(pink)
+
+c6.createButton("ORANGE")
+c6.position(500,200)
+c6.mousePressed(orange)
+
+c7.createButton("YELLOW")
+c7.position(600,200)
+c7.mousePressed(yellow)
+
+c8.createButton("BLACK")
+c8.position(700,200)
+c8.mousePressed(black)
+
+c9.createButton("GRAY")
+c9.position(790,200)
+c9.mousePressed(gray)
+}
+
+function draw(){
+background("white")
+drawSprites()}
+
+function red(){
+    background("red")
+}
+function green(){
+    background("green")
+}
+function blue(){
+    background("blue")
+}
+function purple(){
+    background("purple")
+}
+function pink(){
+    background("pink")
+}
+function orange(){
+    background("orange")
+}
+function yellow(){
+    background("yellow")
+}
+function black(){
+    background("black")
+}
+function gray(){
+    background("gray")
+}
---
+++ sketch.js
@@ -0,0 +1,54 @@
+var c4, c5, c6,c7,c8,c9
+function setup(){
+    createCanvas(800,800)
+//c1=createButton("RED")
+//c1.position (10,200)
+//c1.mousePressed(red)
+
+
+
+c4=createButton("PURPLE")
+c4.position(300,200)
+c4.mousePressed(purple)
+
+c5=createButton("PINK")
+c5.position(400,200)
+c5.mousePressed(pink)
+
+c6=createButton("ORANGE")
+c6.position(500,200)
+c6.mousePressed(orange)
+
+c7=createButton("YELLOW")
+c7.position(600,200)
+c7.mousePressed(yellow)
+
+c8=createButton("BLACK")
+c8.position(700,200)
+c8.mousePressed(black)
+
+c9=createButton("GRAY")
+c9.position(790,200)
+c9.mousePressed(gray)
+}
+
+
+
+function purple(){
+    background("purple")
+}
+function pink(){
+    background("pink")
+}
+function orange(){
+    background("orange")
+}
+function yellow(){
+    background("yellow")
+}
+function black(){
+    background("black")
+}
+function gray(){
+    background("gray")
+}
